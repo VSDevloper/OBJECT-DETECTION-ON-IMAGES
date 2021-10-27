8d38b49a9806e897b72daa06cb9655b354ec57d1
VSDevloper <84179803+VSDevloper@users.noreply.github.com>
2021-10-27 19:32:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,58 @@
+objectDetector= "";
+
 img = "";
+objects = [];
+status = "";
 
 function preload(){
-img = loadImage('Bottel.jpg');
+  img = loadImage('dog_cat.png');
 }
 
-function setup() 
-{
-    canvas = createCanvas(640, 420);
-    canvas.center();
-}
-function draw()
-{
-    image(img, 0, 0, 640, 420);
-fill("#FF0000");
-text("Bottel", 45, 75);
-noFill();
-stroke("#FF0000");
-rect(50, 80, 500, 500 );
+
+function setup() {
+  canvas = createCanvas(380, 380);
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(video, 0, 0, 380, 380);
+
+      if(status != "")
+      {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for (var i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+          document.getElementById("number_of_objects").innerHTML = "Number of objects :" + objects.length;
+    
+          fill(r,g,b);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(r,g,b);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
+}
